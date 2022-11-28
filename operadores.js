601c72fc0fb8600373c10d05bf9b00eec78a7a78
Sebastian <sebasr1268@gmail.com>
2022-11-28 13:04:06 -0500

    añadir codigo html

---
+++ operadores.js
@@ -0,0 +1,8 @@
+//operadores javascript
+console.log(' ejercicio a ')
+console.log(40+2*7**2)
+console.log(' ejercicio b ')
+console.log(3+(2+5)*1+(3-2*3))
+console.log(' ejercicio c ')
+console.log(16-8*(10-(5+4)))
+
